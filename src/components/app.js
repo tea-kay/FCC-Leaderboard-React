6fb1628923002bd7a8882a54c85de4ccb02d836b
Timothy Kim <tkim1202@gmail.com>
2017-10-15 20:46:15 +0900

    Added axios for ajax calls and built helper functions to load camper data

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>Hello World</div>
     );
   }
 }
