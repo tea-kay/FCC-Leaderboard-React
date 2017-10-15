c757427175185f6ca60a4e96ad0a61df1d51c8ce
Timothy Kim <tkim1202@gmail.com>
2017-10-15 20:52:34 +0900

    Added top header and buttons

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentCampers: [],
+      allTimeCampers: [],
+      currentView: 'recentCampers'
+    };
+  }
+  componentWillMount() {
+    axios.all([this.fetchRecentCampers(), this.fetchAllTimCampers()])
+      .then(axios.spread(function(recentCampers, allTimeCampers) {
+        this.setState({ recentCampers, allTimeCampers });
+      }));
+  }
+
+
+  fetchRecentCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+  }
+  fetchAllTimCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+  }
+
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <h2>Display Campers</h2>
+        <button className="btn btn-primary">Recent</button>
+        <button className="btn btn-primary">All Time</button>
+      </div>
     );
   }
 }
